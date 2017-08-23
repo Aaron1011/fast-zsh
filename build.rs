1bdcd890ba94674e72792e0ce894eacbcaf6c14f
Aaron Hill <aa1ronham@gmail.com>
2017-08-23 00:07:14 -0400

    Compile zsh in build.rs

--- build.rs
+++ build.rs
@@ -2,10 +2,13 @@ extern crate bindgen;
 
 use std::path::PathBuf;
 use std::env;
+use std::process::Command;
 
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+
+    compile_zsh();
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -22,4 +25,26 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+
+}
+
+fn compile_zsh() {
+    let old_dir = env::current_dir().unwrap();
+    env::set_current_dir(old_dir.join("zsh")).unwrap();
+
+    Command::new("./Util/preconfig")
+        .spawn()
+        .expect("Failed to run zsh/Util/preconfig");
+
+    Command::new("./configure")
+        .spawn()
+        .expect("Failed to run zsh/configure");
+
+    Command::new("make")
+        .spawn()
+        .expect("Failed to run make");
+
+
+    env::set_current_dir(old_dir).unwrap();
+
 }
